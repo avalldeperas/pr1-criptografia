09ba936d2cca2e08a4321721cb4f9f5bb31010f9
avalldeperas <albertvalldeperas@gmail.com>
2023-03-21 22:09:15 +0100

    avs06 | PR1 | init commit

---
+++ main/P2023_Practica1_Skeleton.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+ABC="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:?"
+
+
+
+# --- IMPLEMENTATION GOES HERE -----------------------------------------------
+#  Student helpers (functions, constants, etc.) can be defined here, if needed
+
+
+
+
+# ----------------------------------------------------------------------------
+
+
+
+
+def uoc_rotative_encrypt(message, shift):
+    """
+    EXERCISE 1: Simple substitution cipher
+    :message: message to cipher (plaintext)
+    :shift: offset or displacement
+    :return: ciphered text
+    """
+
+    ciphertext = ""
+
+    #### IMPLEMENTATION GOES HERE ####
+
+
+
+    # --------------------------------
+
+    return ciphertext
+
+
+def uoc_rotative_decrypt(message, shift):
+    """
+    EXERCISE 2: Simple substitution decipher
+    :message: message to cipher (plaintext)
+    :shift: offset or displacement
+    :return: ciphered text
+    """
+
+    plaintext = ""
+
+    #### IMPLEMENTATION GOES HERE ####
+
+
+    # --------------------------------
+
+    return plaintext
+
+
+
+
+def uoc_grille_genkey(grille_len, num_holes):
+    """
+    EXERCISE 3: Key generation
+    :gruille_len: total grille length in symbols
+    :num_holes: Number of holes in the grille
+    :return: key as list of 0 and 1
+    """
+
+    key = []
+
+    #### IMPLEMENTATION GOES HERE ####
+
+
+    # --------------------------------
+
+    return key
+
+
+
+
+
+
+def uoc_grille_encrypt(key, plaintext):
+    """
+    EXERCISE 4: Encrypt a text using the key
+    :message: message to grille_encrypt
+    :shift: offset or displacement
+    :return: ciphered text
+    """
+
+    ciphertext = ""
+
+    #### IMPLEMENTATION GOES HERE ####
+    
+
+    # --------------------------------
+
+    return ciphertext
+
+
+
+
+def uoc_grille_decrypt(key, ciphertext):
+    """
+    EXERCISE 5: Decrypt a text using the key
+    :message: message to grille_decrypt
+    :subs_alphabet: substitution alphabet
+    :return: ciphered text
+    """
+
+    plaintext = ""
+
+    #### IMPLEMENTATION GOES HERE ####
+
+
+    # --------------------------------
+
+    return plaintext
+
+
+
+
+def uoc_encrypt(key, plaintext):
+    """
+    EXERCISE 6: Complete cryptosystem (encrypt)
+    :key: grille key
+    :plaintext: message to encrypt
+    :return: encrypted text
+    """
+
+    ciphertext = ""
+
+    #### IMPLEMENTATION GOES HERE ####
+
+
+    # --------------------------------
+
+    return ciphertext
+
+
+
+
+def uoc_decrypt(key, ciphertext):
+    """
+    EXERCISE 6: Complete cryptosystem (decrypt)
+    :key: grille key
+    :ciphertext: message to decrypt
+    :return: plaintext
+    """
+
+    plaintext = ""
+
+    #### IMPLEMENTATION GOES HERE ####
+
+
+    # --------------------------------
+
+    return plaintext
+
+
+
+
+
+
+
+
+
+
+
+
